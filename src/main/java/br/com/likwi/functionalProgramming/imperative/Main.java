4bae3492d3014067892959d1da58680cf12c71cf
Renato Fagalde <renato@likwi.com.br>
2021-12-24 11:19:26 -0300

    Imperative approach

---
+++ src/main/java/br/com/likwi/functionalProgramming/imperative/Main.java
@@ -0,0 +1,61 @@
+package br.com.likwi.functionalProgramming.imperative;
+
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static br.com.likwi.functionalProgramming.imperative.Main.Gender.FEMALE;
+
+public class Main {
+
+    static Faker faker = new Faker();
+
+    public static void main(String[] args) {
+        final List<Person> people = List.of(Person.builder()
+                        .name("Ale")
+                        .gender(FEMALE)
+                        .build(),
+                Person.builder()
+                        .name("Roberta")
+                        .gender(FEMALE)
+                        .build(),
+                Person.builder()
+                        .name("Senna")
+                        .gender(Gender.MALE)
+                        .build(),
+                Person.builder()
+                        .name("Neo")
+                        .gender(Gender.MALE)
+                        .build());
+
+        //imeprative
+        List<Person> females = new ArrayList<Person>();
+        for (Person person : people) {
+            if (FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+        for (Person f : females) {
+            System.out.println(f);
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @Builder
+    @ToString
+    static class Person {
+
+        private final String name;
+        private final Gender gender;
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
