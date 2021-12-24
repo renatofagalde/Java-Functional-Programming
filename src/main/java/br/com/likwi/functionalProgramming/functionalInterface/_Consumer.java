84b7496b0150f1ee351cea9d55d4e4b8d52448a6
Renato Fagalde <renato@likwi.com.br>
2021-12-24 12:32:33 -0300

    Consumer

---
+++ src/main/java/br/com/likwi/functionalProgramming/functionalInterface/_Consumer.java
@@ -0,0 +1,52 @@
+package br.com.likwi.functionalProgramming.functionalInterface;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.text.MessageFormat;
+import java.util.function.Consumer;
+
+public class _Consumer {
+
+    static final Customer ale = Customer.builder()
+            .name("Ale")
+            .customerPhoneNumber("99999")
+            .build();
+    static final Customer bia = Customer.builder()
+            .name("Beatriz")
+            .customerPhoneNumber("00000")
+            .build();
+
+
+    public static void main(String[] args) {
+        greetCustomer(ale);
+        greetCustomer(bia);
+        System.out.println("********   Functional Interface  **********");
+        greetCustumerConsumer.accept(ale);
+        greetCustumerConsumer.accept(bia);
+    }
+
+    static Consumer<Customer> greetCustumerConsumer = customer -> System.out.println(MessageFormat.format("Hello {0}, thanks for registering phone number {1}",
+            customer.getName(), customer.getName()));
+
+
+    static void greetCustomer(Customer customer) {
+        final String greet = MessageFormat.format("Hello {0}, thanks for registering phone number {1}", customer.getName(), customer.getName());
+        System.out.println(greet);
+    }
+
+    @Data
+    @AllArgsConstructor
+    @Builder
+    @ToString
+    static class Customer {
+        private final String name;
+        private final String customerPhoneNumber;
+    }
+
+    private static void createCustomers() {
+    }
+
+}
--- src/main/java/br/com/likwi/functionalProgramming/functionalInterface/_Function.java
+++ src/main/java/br/com/likwi/functionalProgramming/functionalInterface/_Function.java
@@ -1,22 +1,41 @@
 package br.com.likwi.functionalProgramming.functionalInterface;
 
 import java.text.MessageFormat;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
-        System.out.println(MessageFormat.format("By regular way {0}",incrementByOne(0)));
+        System.out.println(MessageFormat.format("By regular way {0}", incrementByOne(0)));
 
-        final Integer integer = incrementByOneFunction.apply(1);
-        System.out.println(MessageFormat.format("By function {0}",integer));
+        final Integer incrementByFunction = incrementByOneFunction.apply(1);
+        System.out.println(MessageFormat.format("By function {0}", incrementByFunction));
 
+        final Integer multiply = _Function.multiplyBy10.apply(incrementByFunction);
+        System.out.println("multiply = " + multiply);
+
+        final Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10);
+        System.out.println("addBy1AndThenMultiplyBy10 = " + addBy1AndThenMultiplyBy10.apply(1));
+
+        //the same of incrementByOneMultiply
+        System.out.println("incrementByOneMultiplyBiFunction.apply(4, 100) = " + incrementByOneMultiplyBiFunction.apply(4, 100));
 
     }
+
     //T = Type
     //R = result (return)
     static Function<Integer, Integer> incrementByOneFunction = number -> ++number;
+    static Function<Integer, Integer> multiplyBy10 = number -> number * 10;
 
     static int incrementByOne(int number) {
         return ++number;
     }
+
+    static int incrementByOneMultiply(int number, int numberToMultiplyBy) {
+        return (++number) * numberToMultiplyBy;
+    }
+
+    static BiFunction<Integer, Integer, Integer> incrementByOneMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiplyBy) -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
 }
