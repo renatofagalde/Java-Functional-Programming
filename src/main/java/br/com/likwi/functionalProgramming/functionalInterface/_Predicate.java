fcd90ee6647e465a49dc33c4ef6156b197e1e017
Renato Fagalde <renato@likwi.com.br>
2021-12-24 12:48:00 -0300

    BiPredicates

--- src/main/java/br/com/likwi/functionalProgramming/functionalInterface/_Predicate.java
+++ src/main/java/br/com/likwi/functionalProgramming/functionalInterface/_Predicate.java
@@ -1,5 +1,6 @@
 package br.com.likwi.functionalProgramming.functionalInterface;
 
+import java.text.MessageFormat;
 import java.util.function.Predicate;
 
 public class _Predicate {
@@ -7,12 +8,20 @@ public class _Predicate {
     public static void main(String[] args) {
 
         final String phone07 = "0700";
-        final String phone09 = "0600";
+        final String phone09 = "9999";
         System.out.println(isPhoneNumberValid(phone07));
         System.out.println(isPhoneNumberValid(phone09));
         System.out.println("*************");
         isPhoneNumberValidPredicate.test(phone07);
         isPhoneNumberValidPredicate.test(phone09);
+        System.out.println("*************");
+        System.out.println(MessageFormat.format(
+                "Is phone number is valid and contains number zero = {0}",
+                isPhoneNumberValidPredicate.and(isPhoneNumberContainsZeroPredicate).test(phone07)));
+        System.out.println(MessageFormat.format(
+                "Is phone number is valid and contains number zero = {0}",
+                isPhoneNumberValidPredicate.and(isPhoneNumberContainsZeroPredicate).test(phone09)));
+
     }
 
     static boolean isPhoneNumberValid(String phoneNumber) {
@@ -22,4 +31,5 @@ public class _Predicate {
 
     static Predicate<String> isPhoneNumberValidPredicate = phoneNumber -> phoneNumber.startsWith("07")
             && phoneNumber.length() == 4;
+    static Predicate<String> isPhoneNumberContainsZeroPredicate = phoneNumber -> phoneNumber.contains("0");
 }
